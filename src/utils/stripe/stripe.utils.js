d687db12049f342876ae0eb58b6199e49de07ef8
xingpancanada <xingpancanada@hotmail.com>
2023-02-11 17:14:16 -0500

    use stripe string directly

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,6 +1,6 @@
 import { loadStripe } from "@stripe/stripe-js";
 ////185
 export const stripePromise = loadStripe(
-  process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
-  //"pk_test_51L1ZG7IQvddcHP21lF7pJv4oTeW1gCEHE0l5cqKLWwRB6xkcvOHoQR0NpxVvDRwkQyLhb369JlisoqKB7cFfSgww00sQvTgZu2"
+  //process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
+  "pk_test_51L1ZG7IQvddcHP21lF7pJv4oTeW1gCEHE0l5cqKLWwRB6xkcvOHoQR0NpxVvDRwkQyLhb369JlisoqKB7cFfSgww00sQvTgZu2"
 );
